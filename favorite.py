6384eb7c7e4a9108705580ecf8a1f12d6bfb8659
noo20250988-cloud <noo20250988@std.psut.edu.jo>
2026-01-11 15:52:16 +0300

    favorite Python program.py

--- favorite.py
+++ favorite.py
@@ -1,1 +1,9 @@
+# This program prints my favorite things
+favorite_color = "Blue"
+favorite_food = "Pizza"
+favorite_hobby = "Listening to music"
+
+print("My favorite color is:", favorite_color)
+print("My favorite food is:", favorite_food)
+print("My favorite hobby is:", favorite_hobby)
 
